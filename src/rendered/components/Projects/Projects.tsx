db75916aae178ccc2fd040c3bd48507398b7c1a0
egor-xyz <i@egor.xyz>
2024-03-04 22:25:09 +0200

    feat: Welcome screen

--- src/rendered/components/Projects/Projects.tsx
+++ src/rendered/components/Projects/Projects.tsx
@@ -1,4 +1,6 @@
 import { useMemo } from 'react';
+import { Button, NonIdealState } from '@blueprintjs/core';
+import Devkitty from 'rendered/assets/devkitty.svg?react';
 
 import { useGroups } from 'rendered/hooks/useGroups';
 import { useProjects } from 'rendered/hooks/useProjects';
@@ -12,8 +14,8 @@ import { ProjectsWrapper, Root } from './Projects.styles';
 const others: Group = { fullName: 'Ungrouped', icon: 'folder-open', id: 'ungrouped', name: 'Ungrouped' };
 
 export const Projects = () => {
-  const { groupsWithAliases, selectedGroups, collapsedGroups, toggleCollapsed } = useGroups();
-  const { projects } = useProjects();
+  const { groupsWithAliases, selectedGroups, collapsedGroups, toggleCollapsed, selectAll } = useGroups();
+  const { projects, addProject } = useProjects();
 
   const sortedProjects = useMemo(() => {
     // project group id not in groups
@@ -39,11 +41,49 @@ export const Projects = () => {
     return sortedProjects;
   }, [groupsWithAliases, projects, selectedGroups]);
 
+  const isEmpty = Boolean(selectedGroups.length) && !sortedProjects.length && projects.length > 0;
+
   return (
     <Root>
       <GroupsSelector />
 
       <ProjectsWrapper>
+        {!projects.length && (
+          <NonIdealState
+            action={
+              <Button
+                icon="plus"
+                intent="primary"
+                text="Add one or few repositories"
+                onClick={addProject}
+              />
+            }
+            description="Add your first repository to get started."
+            icon={<Devkitty height={100} />}
+            title="Welcome to Devkitty!"
+          />
+        )}
+
+        {isEmpty && (
+          <NonIdealState
+            action={
+              <Button
+                icon="reset"
+                text="Select all groups"
+                onClick={selectAll}
+              />
+            }
+            description={
+              <>
+                No projects found in <b>{selectedGroups.slice().join(', ')}</b> group
+                {selectedGroups.length > 1 ? 's' : ''}
+              </>
+            }
+            icon="folder-close"
+            title="No projects"
+          />
+        )}
+
         {!selectedGroups.length &&
           projects.map((project) => (
             <Project
