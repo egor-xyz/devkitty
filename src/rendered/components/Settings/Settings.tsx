45bd7329da61e37e47248a076d909218ac021c15
egor-xyz <i@egor.xyz>
2024-03-05 11:00:45 +0200

    fix: integrations

--- src/rendered/components/Settings/Settings.tsx
+++ src/rendered/components/Settings/Settings.tsx
@@ -1,48 +1,36 @@
 import { Tabs } from '@blueprintjs/core';
 
-import { useAppSettings } from 'rendered/hooks/useAppSettings';
-
 import { Root } from './Settings.styles';
 import { SettingsAppearance } from '../SettingsAppearance';
 import { SettingsIntegrations } from '../SettingsIntegrations';
 import { SettingsGroups } from '../SettingsGroups';
 
-export const Settings = () => {
-  const { editors, shells, selectedEditor, selectedShell } = useAppSettings();
+export const Settings = () => (
+  <Root>
+    <Tabs
+      vertical
+      defaultSelectedTabId="appearance"
+    >
+      <Tabs.Tab
+        icon="style"
+        id="appearance"
+        panel={<SettingsAppearance />}
+        title="Appearance"
+      />
 
-  const showIntegrations = Boolean(editors.length && selectedEditor) || Boolean(shells.length && selectedShell);
+      <Tabs.Tab
+        icon="data-lineage"
+        id="integrations"
+        panel={<SettingsIntegrations />}
+        title="Integrations"
+      />
 
-  return (
-    <Root>
-      <Tabs
-        vertical
-        defaultSelectedTabId="appearance"
-      >
-        <Tabs.Tab
-          icon="style"
-          id="appearance"
-          panel={<SettingsAppearance />}
-          title="Appearance"
-        />
-
-        {showIntegrations && (
-          <Tabs.Tab
-            icon="data-lineage"
-            id="integrations"
-            panel={<SettingsIntegrations />}
-            title="Integrations"
-          />
-        )}
-
-        <Tabs.Tab
-          icon="group-objects"
-          id="groups"
-          panel={<SettingsGroups />}
-          title="Groups"
-        />
-
-        <Tabs.Expander />
-      </Tabs>
-    </Root>
-  );
-};
+      <Tabs.Tab
+        icon="group-objects"
+        id="groups"
+        panel={<SettingsGroups />}
+        title="Groups"
+      />
+    </Tabs>
+  </Root>
+);
