fd426cd1dba04b0d77cf564e5b01ad01339f8981
Egor Stronhin <i@egor.xyz>
2024-03-10 21:27:04 +0200

    feat: Drag and Drop Groups

--- src/rendered/components/GroupCollapse/GroupCollapse.tsx
+++ src/rendered/components/GroupCollapse/GroupCollapse.tsx
@@ -1,5 +1,6 @@
 import { Classes, Icon } from '@blueprintjs/core';
-import type { FC } from 'react';
+import { useRef, type FC } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 
 import { Group } from 'types/Group';
 import { Projects } from 'types/project';
@@ -8,15 +9,63 @@ import { useModal } from 'rendered/hooks/useModal';
 import { Project } from '../Project';
 import { GroupBody, GroupTitle, Root } from './GroupCollapse.styles';
 
+const dragType = 'group';
+
 type Props = {
   collapsed: boolean;
   group: Group;
+  index: number;
   onClick: () => void;
   projects: Projects;
 };
 
-export const GroupCollapse: FC<Props> = ({ group, collapsed, onClick, projects }) => {
+export const GroupCollapse: FC<Props> = ({ group, collapsed, onClick, projects, index }) => {
   const { openModal } = useModal();
+
+  const ref = useRef<HTMLDivElement>(null);
+  const [{ handlerId }, drop] = useDrop({
+    accept: dragType,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId()
+      };
+    },
+    hover(item: { id: string; index: number }, monitor) {
+      if (!ref.current) return;
+
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) return;
+
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return;
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return;
+
+      console.log('move group', dragIndex, hoverIndex);
+
+      item.index = hoverIndex;
+    }
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    }),
+    item: () => {
+      return { id: group.id, index };
+    },
+    type: dragType
+  });
+
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
 
   const removeGroup = () => {
     openModal({
@@ -32,7 +81,12 @@ export const GroupCollapse: FC<Props> = ({ group, collapsed, onClick, projects }
   }
 
   return (
-    <Root key={group.id}>
+    <Root
+      data-handler-id={handlerId}
+      key={group.id}
+      ref={ref}
+      style={{ opacity }}
+    >
       <GroupTitle onClick={() => !isEmpty && onClick()}>
         <div className={Classes.ALIGN_LEFT}>
           <Icon icon={group.icon} />{' '}
