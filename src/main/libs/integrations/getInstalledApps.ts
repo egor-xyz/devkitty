050f0b49c6e3280103b948e2e97a51fd94e04eeb
Egor Stronhin <6418221+egor-xyz@users.noreply.github.com>
2025-05-21 21:02:35 +0300

    refactor: use fs readdir for installed apps

--- src/main/libs/integrations/getInstalledApps.ts
+++ src/main/libs/integrations/getInstalledApps.ts
@@ -1,29 +1,13 @@
-import { exec } from 'child_process';
-
-const getAppsSubDirectory = (stdout: string, directory: string): Array<string> => {
-  let stdoutArr = stdout.split(/[(\r\n)\r\n]+/);
-  stdoutArr = stdoutArr
-    .filter((o: any) => o)
-    .map((i: any) => {
-      return `${directory}/${i}`;
-    });
-  return stdoutArr;
-};
-
-const getDirectoryContents = (directory: string): Promise<Array<string>> => {
-  return new Promise((resolve, reject) => {
-    exec(`ls ${directory}`, (error, stdout) => {
-      if (error) {
-        reject(error);
-      } else {
-        try {
-          resolve(getAppsSubDirectory(stdout, directory));
-        } catch (err) {
-          reject(err);
-        }
-      }
-    });
-  });
+import { promises as fs } from 'fs';
+import path from 'path';
+/**
+ * Read application bundles from the given directory.
+ *
+ * This implementation avoids spawning a shell and works cross platform.
+ */
+const getDirectoryContents = async (directory: string): Promise<string[]> => {
+  const entries = await fs.readdir(directory);
+  return entries.map((name) => path.join(directory, name));
 };
 
 export const getInstalledApps = async (directory: string): Promise<string[]> => {
