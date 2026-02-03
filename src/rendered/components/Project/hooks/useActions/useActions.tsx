25724683e53c82dcc3d88871248f5556216a70fa
Egor Stronhin <6418221+egor-xyz@users.noreply.github.com>
2026-02-03 11:14:15 +0200

    feat: pulls (#122)

--- src/rendered/components/Project/hooks/useActions/useActions.tsx
+++ src/rendered/components/Project/hooks/useActions/useActions.tsx
@@ -99,6 +99,7 @@ export const useActions = (gitStatus: GitStatus, project: Project) => {
             <Empty className={Classes.TEXT_MUTED}>
               <span>
                 No actions {inProgress && 'in progress'} were found
+
                 {!all && (
                   <span>
                     {' '}
